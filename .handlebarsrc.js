7251d2da7182a69b3e6a2e75f13cebd2bb3e7c2b
Daniel Abel <daniel@zkreations.com>
2025-02-01 20:38:48 -0500

    chore: update hamlet-builder 1.0.11

--- .handlebarsrc.js
+++ .handlebarsrc.js
@@ -1,15 +1,23 @@
 import Handlebars from 'handlebars'
 
+
+const ternary = (cond, valorTrue, valorFalse) => {
+  return cond ? valorTrue : valorFalse;
+}
+
+const stringify = (context) => {
+  const jsonString = JSON.stringify(context)
+    .replace(/"([^"]+)":/g, '$1:')
+    .replace(/:"/g, ': "')
+    .replace(/&#x3D;/g, '=')
+    .replace(/"(\([^"]+\))"/g, '$1')
+
+  return new Handlebars.SafeString(decodeURI(jsonString))
+}
+
 export default {
   helpers: {
-    stringify: function (context) {
-      const jsonString = JSON.stringify(context)
-        .replace(/"([^"]+)":/g, '$1:')
-        .replace(/:"/g, ': "')
-        .replace(/&#x3D;/g, '=')
-        .replace(/"(\([^"]+\))"/g, '$1')
-
-      return new Handlebars.SafeString(decodeURI(jsonString))
-    }
+    ternary,
+    stringify
   }
 }
